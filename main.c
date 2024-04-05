c42a15fd27793db8ea9e312089f9f17e8d53a88a
leviongit <levi.duncan.self@gmail.com>
2024-04-05 21:54:17 +0200

    worsen the byteswap thingo

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <dragonruby.h>
-#include <endian.h>
+#include <features.h>
 #include <mruby.h>
+#include <stdint.h>
 #include <zlib.h>
+
+static inline uint64_t host_to_big(uint64_t a) {
+  volatile uint64_t chk = 1;
+  if (*(uint8_t *)&chk == 1) {
+    return __builtin_bswap64(a);
+  } else {
+    return a;
+  }
+}
 
 #if defined(__linux__) || defined(__APPLE__)
 #define DL(NAME) NAME
@@ -20,8 +30,9 @@ mrb_value rz_compress_string(mrb_state *mrb, mrb_value self) {
   DL(mrb_get_args)(mrb, "|i", &level);
 
   if (level < -1 || level > 9) {
-		DL(mrb_raisef)(mrb, E_ARGUMENT_ERROR,
-                  "expected `level` to be in the range 0..9, got %d", level);
+    DL(mrb_raisef)
+    (mrb, E_ARGUMENT_ERROR, "expected `level` to be in the range 0..9, got %d",
+     level);
   }
 
   size_t res_len = compressBound(len) + sizeof(size_t);
@@ -31,12 +42,12 @@ mrb_value rz_compress_string(mrb_state *mrb, mrb_value self) {
                      (Bytef *)str, len, level);
 
   if (ec != Z_OK) {
-		DL(mrb_raise)(mrb, mrb->eStandardError_class, "couldn't compress string");
+    DL(mrb_raise)(mrb, mrb->eStandardError_class, "couldn't compress string");
   }
 
   struct RString *res_rstr = RSTRING(res_str);
   _Static_assert(sizeof(size_t) == 8);
-  *(size_t *)RSTR_PTR(res_rstr) = htobe64(len);
+  *(size_t *)RSTR_PTR(res_rstr) = host_to_big(len);
   RSTR_SET_LEN(res_rstr, res_len + sizeof(size_t));
 
   return res_str;
@@ -48,12 +59,13 @@ mrb_value rz_decompress_string(mrb_state *mrb, mrb_value self) {
   const size_t len = (size_t)RSTR_LEN(rstr);
 
   if (len < 8) {
-		DL(mrb_raise)(mrb, mrb->eStandardError_class,
-                 "tried to decompress a string that definitely does not have a "
-                 "length tag");
+    DL(mrb_raise)
+    (mrb, mrb->eStandardError_class,
+     "tried to decompress a string that definitely does not have a "
+     "length tag");
   }
 
-  size_t size = be64toh(*(size_t *)str);
+  size_t size = host_to_big(*(size_t *)str);
   size_t src_size = size;
   size_t expected_size = size;
   str += sizeof(size_t);
@@ -66,18 +78,21 @@ mrb_value rz_decompress_string(mrb_state *mrb, mrb_value self) {
 
   if (ec != Z_OK) {
     if (ec == Z_DATA_ERROR || ec == Z_BUF_ERROR) {
-			DL(mrb_raise)(mrb, mrb->eStandardError_class,
-                   "failed to decompress string, corrupted data");
+      DL(mrb_raise)
+      (mrb, mrb->eStandardError_class,
+       "failed to decompress string, corrupted data");
     } else {
-			DL(mrb_raise)(mrb, mrb->eStandardError_class,
-                   "failed to decompress string, memory alloc failiure");
+      DL(mrb_raise)
+      (mrb, mrb->eStandardError_class,
+       "failed to decompress string, memory alloc failiure");
     }
   }
 
   if (size != expected_size) {
-		DL(mrb_raisef)(mrb, mrb_exc_get_id(mrb, mrb_intern_lit(mrb, "IndexError")),
-                  "size mismatch (data corrupted) (expected %d, got %d)",
-                  expected_size, size);
+    DL(mrb_raisef)
+    (mrb, mrb_exc_get_id(mrb, mrb_intern_lit(mrb, "IndexError")),
+     "size mismatch (data corrupted) (expected %d, got %d)", expected_size,
+     size);
   }
 
   RSTR_SET_LEN(res_rstr, size);
@@ -86,10 +101,12 @@ mrb_value rz_decompress_string(mrb_state *mrb, mrb_value self) {
 }
 
 void drb_register_c_extensions_with_api(mrb_state *mrb, struct drb_api_t *api) {
-	drb = api;
+  drb = api;
   struct RClass *str = mrb->string_class;
-  DL(mrb_define_method_id)(mrb, str, mrb_intern_lit(mrb, "compress"),
-                          rz_compress_string, MRB_ARGS_OPT(1));
-  DL(mrb_define_method_id)(mrb, str, mrb_intern_lit(mrb, "decompress"),
-                          rz_decompress_string, MRB_ARGS_NONE());
+  DL(mrb_define_method_id)
+  (mrb, str, mrb_intern_lit(mrb, "compress"), rz_compress_string,
+   MRB_ARGS_OPT(1));
+  DL(mrb_define_method_id)
+  (mrb, str, mrb_intern_lit(mrb, "decompress"), rz_decompress_string,
+   MRB_ARGS_NONE());
 }
